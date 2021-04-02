4f18d84c48fe5df6b0748d7368ddcc907e8dab7f
osvelvargas <osvelvargas@gmail.com>
2021-04-02 09:37:34 -0400

    Log send email queues

--- resources/views/email/tagDomicilio.blade.php
+++ resources/views/email/tagDomicilio.blade.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <!--  <title>How to send mail using queue in Laravel 6? - ItSolutionStuff.com</title> -->
+    <!--  <title>How to send mail using queue in Laravel 6? - ItSolutionStuff.com</title> -->
 </head>
 <body>
 <p>Estimado(a) {{ isset($data['cliente']) ? $data['cliente'] : "" }}</p>
-<p>Su solicitud de servicio <strong>{{ $data['servicio'] }}</strong> se ha recibido exitosamente.  @if ($data['existe_cod_descuento'] != true )Para continuar con el servicio, es necesario <a href="{{ $data['linkpayment'] }}">realizar el pago</a>. @endif Para consultar el estado de la gestión, ingrese a
-    <a href="https://tad.integrocorp.cl/{{ $data['url_abbr'] }}">nuestro sitio</a> y en la sección "Consulte el estado de su solicitud", digite el número de rut asociado a la
+<p>Su solicitud de servicio <strong>{{ $data['servicio'] }}</strong> se ha recibido
+    exitosamente. @if ($data['existe_cod_descuento'] != true )  @if( isset($data['url_abbr']) && ($data['url_abbr']=="cn" || $data['url_abbr']=="avs" ))
+        Se revisarán sus documentos y en menos de 24 horas hábiles se le confirmará que estos estén correctos para que
+        realice el pago. @else Para continuar con el servicio, es necesario <a href="{{ $data['linkpayment'] }}">realizar
+            el pago</a>. @endif @endif Para consultar el estado de la gestión, ingrese a
+    <a href="https://tad.integrocorp.cl/{{ $data['url_abbr'] }}">nuestro sitio</a> y en la sección "Consulte el estado
+    de su solicitud", digite el número de rut asociado a la
     solicitud y el número de ticket.</p>
 <p><strong>RUT</strong> {{ $data['rut'] }}</p>
 <p><strong>No. de ticket</strong> {{ $data['ticket'] }}</p><br>
@@ -14,6 +19,7 @@
 <p>Atentamente,</p>
 <p>TAD Equipo Atención al cliente</p><br>
 
-<small>Este mail se ha enviado automáticamente, por favor no responda al mismo.</small><br>
+<small>Este mail se ha enviado automáticamente, por favor no responda al mismo.</small>
+<br>
 </body>
 </html>
